3581e7ca3222a6e3d43ddf6d071b5db8654c355f
bryanarboledaa <jbdarboleda@gmail.com>
2023-01-27 16:23:02 +0800

    added uniqueness validation in book model for isbn10 and isbn13

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ApplicationRecord
     has_many :joiners, dependent: :destroy
     has_many :authors, through: :joiners
     belongs_to :publisher
+
+    validates :isbn_10, uniqueness: true
+    validates :isbn_13, uniqueness: true
 end
